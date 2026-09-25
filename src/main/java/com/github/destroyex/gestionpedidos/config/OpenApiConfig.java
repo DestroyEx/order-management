fdf2c3e265495f5f36aa6abc94af524af9a6910c
David Szabo <davidszabo870@gmail.com>
2026-09-25 02:08:38 +0200

    feat: add OpenAPI config

---
+++ src/main/java/com/github/destroyex/gestionpedidos/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.github.destroyex.gestionpedidos.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPi() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
