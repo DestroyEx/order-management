d7e00f86017a89bc16e6ff52f85aa6c38736609e
David Szabo <davidszabo870@gmail.com>
2026-09-25 02:10:13 +0200

    fix: add missing repository annotation to OrderRepository and OrderLineRepository

--- src/main/java/com/github/destroyex/gestionpedidos/dao/OrderLineRepository.java
+++ src/main/java/com/github/destroyex/gestionpedidos/dao/OrderLineRepository.java
@@ -2,6 +2,8 @@ package com.github.destroyex.gestionpedidos.dao;
 
 import com.github.destroyex.gestionpedidos.entity.OrderLine;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderLineRepository extends JpaRepository<OrderLine, Long> {
 }
--- src/main/java/com/github/destroyex/gestionpedidos/dao/OrderRepository.java
+++ src/main/java/com/github/destroyex/gestionpedidos/dao/OrderRepository.java
@@ -2,6 +2,8 @@ package com.github.destroyex.gestionpedidos.dao;
 
 import com.github.destroyex.gestionpedidos.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
